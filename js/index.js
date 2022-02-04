edf274a5b8a0ce369e4532e70677c092d09bd7fc
Owen <43004995+UnintelliJ@users.noreply.github.com>
2022-02-04 18:10:43 -0500

    Homepage finished

--- js/index.js
+++ js/index.js
@@ -1,35 +1,58 @@
-window.onload = function(){
-    var wrapper = document.querySelector(".wrapper");
-    var text = document.querySelector(".text");
-    
-    var textCont = text.textContent;
-    text.style.display = "none";
-    
-    for (var i = 0; i < textCont.length; i++) {
-      (function(i) {
-        setTimeout(function() {
-          // Created textNode to append
-          var texts = document.createTextNode(textCont[i])
-          var span = document.createElement('span');
-          span.appendChild(texts);
-    
-          span.classList.add("wave");
-          wrapper.appendChild(span);
-    
-        }, 75 * i);
-      }(i));
-    }    
+
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-
-    // Or with jQuery
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-  });
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
   
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 5000);
+
+
